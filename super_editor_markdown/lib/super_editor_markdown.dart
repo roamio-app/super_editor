d7769db6c437e557d7dd8ab14234a79b2c2ad5a0
Sang Doan <std35@georgetown.edu>
2024-02-21 18:42:55 -0500

    Update super_editor_markdown.dart

--- super_editor_markdown/lib/super_editor_markdown.dart
+++ super_editor_markdown/lib/super_editor_markdown.dart
@@ -2,3 +2,5 @@ export 'src/document_to_markdown_serializer.dart';
 export 'src/markdown_to_attributed_text_parsing.dart';
 export 'src/markdown_to_document_parsing.dart';
 export 'src/super_editor_syntax.dart';
+export 'src/super_editor_paste_markdown.dart';
+export 'src/image_syntax.dart';
